6a4b99e3dcbdd24fb6ed99edcf5c098a3c05c49a
kondakova2 <141423621+konda23@users.noreply.github.com>
2026-06-17 15:25:09 -0400

    Update Ejercicio-N4.cpp

--- Ejercicio-N4.cpp
+++ Ejercicio-N4.cpp
@@ -1,6 +1,5 @@
-#include <stdio.h>  
+#include <iostream>  
 #include <windows.h>  
-#include<iostream>
 using namespace std;
  void gotoxy(int x,int y){  
       HANDLE hcon;  
@@ -12,8 +11,8 @@ using namespace std;
  }  
  
  void cuadrado(int x, int y){
- 	for(int i=0 ; i<10 ; i++){
- 		for(int j=0 ; j<10; j++){
+ 	for(int i=0 ; i<6 ; i++){
+ 		for(int j=0 ; j<6; j++){
  			cout<<"*";
 			 
 		 }
@@ -32,18 +31,55 @@ using namespace std;
  }
  
   
- void triangulo(int x, int y){
- 	for(int i=0 ; i<10 ; i++){
- 		for(int j=0 ; j<5; j++){
- 			if()
+ void triangulo(int x, int y, int n){
+ 	for(int i=0 ; i<n ; i++){
+ 		for(int j=0 ; j<n; j++){
+ 			if(i==j || j==0 || i==n-1){
+ 				gotoxy(x+j,y+i);
+				 cout<<"*";
+			 }
 		 }
-		 cout<<endl;
 	 }
  }
  
  int main(){
- 	int x=0,y=0;
+ 	int x=0,y=0,n=10;
+ 	int figura=0;
+ 	int selec= 0;
  	
- 	triangulo(x,y);
+ 	do{
+ 		
+ 	cout<<"Seleccione la figura que desea ver:"<<endl;
+ 	cout<<"----------------------------------"<<endl;
+ 	cout<<"1. cuadrado"<<endl;
+ 	cout<<"2. Triangulo"<<endl;
+ 	cout<<"3. Rectangulo"<<endl;
+ 	cin>>figura;
  	
+ 	system("cls");
+ 	switch(figura){
+ 		case 1:
+ 			cuadrado(x,y);
+ 			break;
+ 		case 2:
+ 			triangulo(x,y,n);
+ 			cout<<""<<endl;
+ 			break;
+ 		case 3:
+ 			rectangulo(x,y);
+ 			break;
+ 		default:
+ 			cout<<"Error, opcion no disponible"<<endl;
+			break;
+	 }
+	 
+	 cout<<"Desea ver otra figura?"<<endl;
+	 cout<<"Si (1)"<<endl;
+	 cout<<"No (2)"<<endl;
+	 cin>>selec;
+	 system("cls");
+	 
+	 }while(selec !=2);
+	 
+	 return 0;
  }
